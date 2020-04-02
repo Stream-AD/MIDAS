01457e87de892c4497bb62aa9980115af1703b31
Rui LIU <xxliuruiabc@gmail.com>
2020-04-02 17:23:44 +0800

    + custom output path.

--- util/ReproduceROC.py
+++ util/ReproduceROC.py
@@ -1,18 +1,19 @@
 from pathlib import Path
 from sys import argv
 
-from numpy import savetxt, array
+from numpy import array, savetxt
 from pandas import read_csv
 from sklearn.metrics import auc, roc_curve
 
 root = (Path(__file__) / '../..').resolve()
 
 if len(argv) < 3:
-	print('Print ROC-AUC to stdout and save points on ROC curve to RejectMIDAS/temp/ROC.csv')
-	print('Usage: python ReproduceROC.py <pathGroundTruth> <pathScore>')
+	print('Print ROC-AUC to stdout and save points on ROC curve to RejectMIDAS/<pathROC> (default temp/ROC.csv)')
+	print('Usage: python ReproduceROC.py <pathGroundTruth> <pathScore> [<pathROC>]')
 else:
 	y = read_csv(argv[1], header=None)
 	z = read_csv(argv[2], header=None)
 	fpr, tpr, _ = roc_curve(y, z, pos_label=1)
 	print(f"ROC-AUC = {auc(fpr, tpr):.4f}")
-	savetxt(root / 'temp/ROC.csv', array([fpr, tpr]).T, fmt='%f', delimiter=',')
+	pathROC = argv[3] if len(argv) >= 4 else 'temp/ROC.csv'
+	savetxt(root / pathROC, array([fpr, tpr]).T, fmt='%f', delimiter=',')
