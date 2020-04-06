7ea415900d21340e12329949080072ac84dc26d1
Rui LIU <xxliuruiabc@gmail.com>
2020-04-06 14:31:27 +0800

    + scalability experiments.

--- example/AttackExperiment.cpp
+++ example/AttackExperiment.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
+#include <chrono>
 
 #include <tbb/parallel_for.h>
 
@@ -99,6 +100,58 @@ void ReproduceROC(int n, const char pathGroundTruth[], int numColumn, float thre
 	delete[] score;
 }
 
+void TestEdgeScalability(int numColumn, float threshold, const std::vector<int>& numsRecord, int numRepeat, const int source[], const int destination[], const int timestamp[]) {
+	const auto time = new long long[numsRecord.size() * numRepeat];
+	const auto seed = new int[numRepeat];
+	std::for_each(seed, seed + numRepeat, [](int& a) { a = rand(); });
+	for (int i = 0; i < numsRecord.size(); i++) {
+		for (int j = 0; j < numRepeat; j++) {
+			srand(seed[j]);
+			// RejectMIDAS::CPU::NormalCore midas(2, numColumn, threshold);
+			RejectMIDAS::CPU::RelationalCore midas(2, numColumn, threshold);
+			const auto timeBegin = std::chrono::high_resolution_clock::now();
+			for (int k = 0; k < numsRecord[i]; k++)
+				midas(source[k], destination[k], timestamp[k]);
+			printf("Time%02d = %lldus\n", j, time[i * numRepeat + j] = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - timeBegin).count());
+		}
+		printf("// Above results use numRecord = %d\n", numsRecord[i]);
+	}
+	const auto fileExperimentResult = fopen(SOLUTION_DIR"temp/Experiment.csv", "w");
+	fprintf(fileExperimentResult, "numColumn,threshold,numRecord,seed,time\n"); // Microsecond (us), other tests are millisecond (ms)
+	for (int i = 0; i < numsRecord.size(); i++)
+		for (int j = 0; j < numRepeat; j++)
+			fprintf(fileExperimentResult, "%d,%g,%d,%d,%lld\n", numColumn, threshold, numsRecord[i], seed[j], time[i * numRepeat + j]);
+	fclose(fileExperimentResult);
+	delete[] time;
+	delete[] seed;
+}
+
+void TestColumnScalability(int n, const std::vector<int>& numsColumn, float threshold, int numRepeat, const int source[], const int destination[], const int timestamp[]) {
+	const auto time = new long long[numsColumn.size() * numRepeat];
+	const auto seed = new int[numRepeat];
+	std::for_each(seed, seed + numRepeat, [](int& a) { a = rand(); });
+	for (int i = 0; i < numsColumn.size(); i++) {
+		for (int j = 0; j < numRepeat; j++) {
+			srand(seed[j]);
+			// RejectMIDAS::CPU::NormalCore midas(2, numsColumn[i], threshold);
+			RejectMIDAS::CPU::RelationalCore midas(2, numsColumn[i], threshold);
+			const auto timeBegin = std::chrono::high_resolution_clock::now();
+			for (int k = 0; k < n; k++)
+				midas(source[k], destination[k], timestamp[k]);
+			printf("Time%02d = %lldus\n", j, time[i * numRepeat + j] = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - timeBegin).count());
+		}
+		printf("// Above results use numColumn = %d\n", numsColumn[i]);
+	}
+	const auto fileExperimentResult = fopen(SOLUTION_DIR"temp/Experiment.csv", "w");
+	fprintf(fileExperimentResult, "numColumn,threshold,seed,time\n"); // Microsecond (us), other tests are millisecond (ms)
+	for (int i = 0; i < numsColumn.size(); i++)
+		for (int j = 0; j < numRepeat; j++)
+			fprintf(fileExperimentResult, "%d,%g,%d,%lld\n", numsColumn[i], threshold, seed[j], time[i * numRepeat + j]);
+	fclose(fileExperimentResult);
+	delete[] time;
+	delete[] seed;
+}
+
 int main(int argc, char* argv[]) {
 	// Parameter
 	// --------------------------------------------------------------------------------
@@ -143,6 +196,12 @@ int main(int argc, char* argv[]) {
 	TestAUC(n, pathGroundTruth, numColumn, thresholds, numRepeat, source, destination, timestamp);
 	// TestSpeed(n, numColumn, thresholds, numRepeat, source, destination, timestamp);
 	// ReproduceROC(n, pathGroundTruth, numColumn, 1000, 8918, source, destination, timestamp);
+
+	const auto numsRecord = {1 << 10, 1 << 11, 1 << 12, 1 << 13, 1 << 14, 1 << 15, 1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22, 1 << 23};
+	// TestEdgeScalability(numColumn, 10000, numsRecord, numRepeat, source, destination, timestamp);
+
+	const auto numsColumn = {600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000};
+	// TestColumnScalability(n, numsColumn, 10000, numRepeat, source, destination, timestamp);
 
 	// Clean up
 
