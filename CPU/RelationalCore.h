28b23fbc6dfe962e82148f73dae2d96e1b2a7ab8
Rui LIU <xxliuruiabc@gmail.com>
2020-05-16 04:47:18 +0800

    - useless comment.

--- CPU/RelationalCore.h
+++ CPU/RelationalCore.h
@@ -36,7 +36,7 @@ struct RelationalCore {
 	}
 
 	static float ComputeScore(float a, float s, float t) {
-		return s == 0 || t - 1 == 0 ? 0 : pow((a - s / t) * t, 2) / (s * (t - 1)); // If t == 1, then s == 0, so no need to check twice
+		return s == 0 || t - 1 == 0 ? 0 : pow((a - s / t) * t, 2) / (s * (t - 1));
 	}
 
 	float operator()(int source, int destination, int timestamp) {
