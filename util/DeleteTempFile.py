9e2187a1cd5c23bc11df6c2e760db2ae5aa8be3b
Rui LIU <xxliuruiabc@gmail.com>
2020-06-15 18:51:03 +0800

    Copyright owner + Siddharth Bhatia.

--- util/DeleteTempFile.py
+++ util/DeleteTempFile.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright 2020 Rui LIU
+# Copyright 2020 Rui Liu (liurui39660) and Siddharth Bhatia (bhatiasiddharth)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
